0e3e2efe3aa97b50e1c0c1fc080f8b95f41f09a5
chago1991 <chago19911@gmail.com>
2024-01-30 13:43:49 +0400

    new version

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-REPO=$1
-TOKEN=$2
+REPO=$ORG
+TOKEN=$TOKEN
 
 RUNNER_LIST=$(curl -H "Authorization: Bearer ${TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/${REPO}/actions/runners | jq '[.runners[] | select(.status | contains("offline")) | {id: .id}]')
 
@@ -10,5 +10,5 @@ for id in $(echo "$RUNNER_LIST" | jq -r '.[] | @base64'); do
                 echo ${id} | base64 --decode | jq -r ${1}
         }
         echo $(_jq '.id')
-        curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/ORG/actions/runners/$(_jq '.id')
+        curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/${REPO}/actions/runners/$(_jq '.id')
 done
--- start.sh
+++ start.sh
@@ -9,19 +9,6 @@ echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 ####RE
 REG_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/satu-com-ge/actions/runners/registration-token | jq .token --raw-output)
 
-shutdown_signal_handler() {
-    echo "Received shutdown signal. Waiting 30 seconds before cleanup..."
-    sleep 20
-    echo "Executing cleanup.sh"
-    ./cleanup.sh
-    exit 0
-}
-
-# Trap the shutdown signal and call the handler function
-trap 'shutdown_signal_handler' SIGTERM
-
-
-
 cd /home/docker/actions-runner
 
 #./config.sh --url https://github.com/${ORG} --token ${REG_TOKEN}
@@ -30,6 +17,8 @@ cd /home/docker/actions-runner
 cleanup() {
     echo "Removing runner..."
     ./config.sh remove --unattended --token ${REG_TOKEN}
+    echo "Executing cleanup.sh"
+    ./cleanup.sh
 }
 
 trap 'cleanup; exit 130' INT
